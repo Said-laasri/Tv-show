b3a0ed7a05c2099470786a92fbd7f4b800469c92
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-07-27 18:56:14 +0200

    Add Reservation section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './assets/img/logo.png';
 import uploadLikes from './post.js';
 import getLikes from './gitlikes.js';
 import counter from './apicounter.js';
+import './reservation.css';
 
 window.onload = () => {
   renderAPI();
